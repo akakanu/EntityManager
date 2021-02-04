8d5fb7d5d57869361ed3153f26ac07c9128e775f
akakanu <lakakanu@yahoo.fr>
2021-02-03 20:55:57 -0500

    ajout  notation Id

---
+++ EntityManager/Annotations/Id.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace EntityManager.Annotations
+{
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Property)]
+    [ComVisible(true)]
+    public class Id : Attribute
+    {
+        private bool auto = true;
+        public bool AutoIncrement { get { return auto; } set { auto = value; } }
+
+    }
+}
--- EntityManager/DataBase.cs
+++ EntityManager/DataBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityManager.Annotations;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
@@ -12,19 +13,77 @@ namespace EntityManager
         {
             try
             {
-                foreach(PropertyInfo field in GetType().GetProperties())
+                foreach (PropertyInfo field in GetType().GetProperties())
                 {
-                    Console.WriteLine(field);
+                    string query = "create table if not exist " + TableName(field.PropertyType) + "(";
                     foreach (PropertyInfo info in field.PropertyType.GetProperties(flag))
                     {
-                        Console.WriteLine("colonne :" + info);
+                        query += ColonnName(info) + " " + TypeName(info) + ",";
+
                     }
+                    query = query.Substring(0, query.Length - 1);
+                    query += ")";
+                    Console.WriteLine(query);
+
                 }
             }
             catch (Exception e)
             {
 
             }
+        }
+
+        private string TableName(Type type)
+        {
+            Object obj = type.GetCustomAttribute(typeof(Table));
+            if (obj != null ? obj is Table : false)
+            {
+                return (obj as Table).Name;
+            }
+
+            return null;
+        }
+
+        private string ColonnName(PropertyInfo property)
+        {
+            Object obj = property.GetCustomAttribute(typeof(Colonne));
+            if (obj != null ? obj is Colonne : false)
+            {
+                return (obj as Colonne).Name;
+            }
+            return null;
+        }
+
+        private string TypeName(PropertyInfo prop)
+        {
+            Object key = prop.GetCustomAttribute(typeof(Id));
+            Type type = prop.PropertyType;
+            string result = null;
+            if (type == typeof(Int16) || type == typeof(Int32))
+            {
+                result = (key != null ? (key as Id).AutoIncrement ? "serial" : "integer" : "integer");
+            }
+            else if (type == typeof(String))
+            {
+                result = "character varying";
+            }
+            else if (type == typeof(Boolean))
+            {
+                result = "boolean";
+            }
+            else if (type == typeof(Double))
+            {
+                result = "double precision";
+            }
+            else if (type == typeof(DateTime))
+            {
+                result = "date";
+            }
+            else if (type == typeof(Int64))
+            {
+                result = (key != null ? (key as Id).AutoIncrement ? "bigserial" : "bigint" : "bigint");
+            }
+            return result + (key != null ? " primary key" : "");
         }
     }
 }
